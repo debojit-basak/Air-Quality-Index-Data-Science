3cfdd0ae0d6a5da3906c3f0bdbcad325bd068de4
debojit-basak <39153154+debojit-basak@users.noreply.github.com>
2022-10-30 22:26:51 +0600

    solved yesterday's problem

--- one.py
+++ one.py
@@ -9,19 +9,14 @@ print("hello")
 def retrieve_html():
     for year in range(2013, 2022):
         for month in range(1, 13):
-            if(month < 10):
-                url = 'https://en.tutiempo.net/climate/0%i-%i/ws-432950.html'.format(
-                    month, year)
-            else:
-                url = 'https://en.tutiempo.net/climate/%i-%i/ws-432950.html'.format(
-                    month, year)
+            url = f'https://en.tutiempo.net/climate/{month:02d}-{year}/ws-432950.html'
 
         texts = requests.get(url)
-        text_utf = texts.encode('utf-8')
+        text_utf = texts.text.encode('utf-8')
 
-        if not os.path.exists("/Data/Html_Data/{}".format(year)):
-            os.makedirs("/Data/Html_Data/{}".format())
-        with open("/Data/Html_Data/{}/{}.html".format(year, month), "wb") as output:
+        if not os.path.exists("./Data/Html_Data/{}".format(year)):
+            os.makedirs("./Data/Html_Data/{}".format(year))
+        with open("./Data/Html_Data/{}/{}.html".format(year, month), "wb") as output:
             output.write(text_utf)
 
         sys.stdout.flush()
